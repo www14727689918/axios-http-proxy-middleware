63a49be74e02daf02bcd032747fd9d3cf1959f4c
wangwenyi <236680376@qq.com>
2017-08-04 12:53:03 +0800

    test

---
+++ newapp/index.js
@@ -0,0 +1,64 @@
+var express = require('express')
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var proxy = require('http-proxy-middleware');
+// var pool  = mysql.createPool({
+//     connectionLimit : 10,
+//     host     : process.env.MYSQL_HOST,
+//     port     : process.env.MYSQL_PORT,
+//     user     : process.env.ACCESSKEY,
+//     password : process.env.SECRETKEY,
+//     database : 'app_' + process.env.APPNAME,
+//
+// });
+var connection = mysql.createConnection({
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : '',
+    database : 'kaifanla_new',
+    connectionLimit: 3
+})
+// var connection = mysql.createConnection({
+//     host     : process.env.MYSQL_HOST,
+//     port     : process.env.MYSQL_PORT,
+//     user     : process.env.ACCESSKEY,
+//     password : process.env.SECRETKEY,
+//     database : 'app_' + process.env.APPNAME,
+//     connectionLimit: 3
+// })
+connection.connect();
+var app = express()
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use('/api/:page', proxy({target: 'http://www.jeasyui.com/demo/main/datagrid2_getdata.php', changeOrigin: true}));
+app.get('/', function (req, res) {
+    res.redirect('index.html')
+})
+//放借口文件
+app.get('/dish',function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
+    connection.query('SELECT * FROM kf_dish', function (error, results, fields) {
+        if (error) throw error;
+        // console.log('The solution is: ', results[0]);
+        //var results=results;
+        response.end("callback"+"("+JSON.stringify(results)+")");
+    })
+});
+app.post('/user',function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
+    // console.log(request.body);
+    var uname=request.body.uname;
+    var pwd=request.body.pwd;
+    connection.query(`SELECT * FROM kf_users where uname='${uname}' and pwd='${pwd}'`, function (error, results, fields) {
+        if (error) throw error;
+        // console.log('The solution is: ', results[0]);
+        //var results=results;
+        response.end("callback"+"("+JSON.stringify(results)+")");
+    })
+});
+// app.post('/dsf',function (req,res) {
+//     res.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
+//     res.end('11111')
+// })
+app.listen(process.env.PORT || 5050)
